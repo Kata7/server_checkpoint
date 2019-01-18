8ff83d75d102998617fd0da8d278f534a28108e4
Austin Moran <a.moran3257@gmail.com>
2019-01-18 13:32:14 -0700

    attempting to host on heroku

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 8000
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
 // Import data from data.json
-const data = require('./data.json')
+let data = require('./data.json')
 
 // If no tag, return all data
 app.get('/', (req, res) => res.send(data))
 
 // Send data specified by user-submitted tag
-app.get('/:tag', (req, res) => {
+app.get('/:genre', (req, res) => {
+  const genre = req.params.genre.toLowerCase()
+  const filteredList = data.albums.filter(album => album.genre.includes(genre))
+  if (filteredList.length !== 0) {
+    res.status(200).send(filteredList)
+  } else {
+    res.status(404).send(`Try one of these genres: ${data.genres}`)
+  }
 
 })
 
+// This is a skeleton post method
+app.post('/', (req, res, next) => {
+  res.status(200).send('Thank you for the post')
+  console.log(req.body)
+})
 
 
 // If route does not exist, send 404 error
@@ -21,4 +39,4 @@ app.use((req, res, next) => {
 })
 
 // Allow the server to receive requests
-app.listen(port, () => console.log(`Test server is listening on port: ${port}`))
+app.listen(port, () => console.log(`Server is listening on port: ${port}`))
